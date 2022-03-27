1ccb5d943dfd1ee03cd2f93a6736bec029428c4e
Tonaplo <5838658+Tonaplo@users.noreply.github.com>
2022-03-27 08:32:42 +0200

    Problem 4 added not solved

---
+++ CommonProblems/ProjectEuler/Problem4.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Problem4.h"
+
+Problem4::Problem4()
+{
+
+Problem4::~Problem4()
+{
+}
+
+void Problem4::Execute()
+{}
---
+++ CommonProblems/ProjectEuler/Problem4.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "BaseProblem.h"
+class Problem4 :
+	public BaseProblem
+{
+public:
+	Problem4();
+	~Problem4();
+	void Execute() override;
+};
